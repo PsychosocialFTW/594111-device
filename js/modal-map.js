a18df4ab6555fbbb0cfc27ec1c9baba9864fd48f
PsychosocialFTW <shelshelven@gmail.com>
2018-03-29 23:37:32 +0300

    Завершение верстки

--- js/modal-map.js
+++ js/modal-map.js
@@ -1,8 +1,23 @@
 var mapLink = document.querySelector(".static-map");
-var mapOpen = document.querySelector(".modal-map");
+var mapOpen = document.querySelector(".map-popup");
 var mapClose = document.querySelector(".map-close-btn");
 
 mapLink.addEventListener("click", function (evt) {
- evt.preventDefault();
- mapOpen.classList.add("map-show");
+	evt.preventDefault();
+	mapOpen.classList.add("map-show");
 }); 
+
+mapClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	mapOpen.classList.remove("map-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+
+		if (mapOpen.classList.contains("map-show")) {
+			mapOpen.classList.remove("map-show");
+		}
+	}
+});
