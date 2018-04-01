4baa1869ace162f254ddf0ce6d69a173a0efe4c1
PsychosocialFTW <shelshelven@gmail.com>
2018-04-01 15:08:06 +0300

    js-slider rebuild

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,24 @@
-var slideOne = document.querySelector(".js-delivery-btn");
-var slideTwo = document.querySelector(".js-guarantee-btn");
-var slideThree = document.querySelector(".js-credit-btn");
-var slideOneDelivery = document.querySelector(".delivery-slide");
-var slideTwoGuarantee = document.querySelector(".guarantee-slide");
-var slideThreeCredit = document.querySelector(".credit-slide");
+var toggle1 = document.querySelector('#js-toggle-1');
+var toggle2 = document.querySelector('#js-toggle-2');
+var toggle3 = document.querySelector('#js-toggle-3');
 
-slideTwo.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	slideOne.classList.remove("active");
-	slideThree.classList.remove("active");
-	slideOneDelivery.classList.remove("js-show-slide");
-	slideThreeCredit.classList.remove("js-show-slide");
-	slideTwo.classList.add("active");
-	slideTwoGuarantee.classList.add("js-show-slide");
+var switchSlide = function (slide) {
+	var activeToggle = slide.getAttribute('id').slice(10);
+
+	document.querySelector('.service-btn.active').classList.remove('active');
+	document.querySelector('.services-slider-item.js-show-slide').classList.remove('js-show-slide');
+	slide.classList.add('active');
+	document.querySelector('#js-slider-' + activeToggle).classList.add('js-show-slide');
+};
+
+toggle1.addEventListener('click', function() {
+	switchSlide(this);
 });
 
-slideOne.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	slideTwo.classList.remove("active");
-	slideThree.classList.remove("active");
-	slideTwoGuarantee.classList.remove("js-show-slide");
-	slideThreeCredit.classList.remove("js-show-slide");
-	slideOne.classList.add("active");
-	slideOneDelivery.classList.add("js-show-slide");
+toggle2.addEventListener('click', function() {
+	switchSlide(this);
 });
 
-slideThree.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	slideOne.classList.remove("active");
-	slideTwo.classList.remove("active");
-	slideOneDelivery.classList.remove("js-show-slide");
-	slideTwoGuarantee.classList.remove("js-show-slide");
-	slideThree.classList.add("active");
-	slideThreeCredit.classList.add("js-show-slide");
+toggle3.addEventListener('click', function() {
+	switchSlide(this);
 });
